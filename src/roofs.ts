8597f479aca5114232e50c7b42025a7d6bc5a9f1
Hugo Averty <hugoaverty@gmail.com>
2023-08-02 13:43:33 +0200

    Adding silent overlay

--- src/roofs.ts
+++ src/roofs.ts
@@ -8,32 +8,38 @@ WA.onInit().then(() => {
         WA.room.hideLayer("roof1");
         WA.room.hideLayer("walls-bg-front1");
         WA.room.hideLayer("sign1");
-      });
+    });
     WA.room.onLeaveLayer("floor1").subscribe(() => {
         WA.room.showLayer("roof1");
         WA.room.showLayer("walls-bg-front1");
         WA.room.showLayer("sign1");
-      });
+    });
     WA.room.onEnterLayer("floor2").subscribe(() => {
         WA.room.hideLayer("roof2");
         WA.room.hideLayer("walls-bg-front2");
         WA.room.hideLayer("sign2");
-      });
+    });
     WA.room.onLeaveLayer("floor2").subscribe(() => {
         WA.room.showLayer("roof2");
         WA.room.showLayer("walls-bg-front2");
         WA.room.showLayer("sign2");
-      });
+    });
     WA.room.onEnterLayer("rooms_floor").subscribe(() => {
         WA.room.hideLayer("facade-furniture-bg");
         WA.room.hideLayer("facade-furniture-fg");
         WA.room.hideLayer("facade");
-      });
+    });
     WA.room.onLeaveLayer("rooms_floor").subscribe(() => {
         WA.room.showLayer("facade-furniture-bg");
         WA.room.showLayer("facade-furniture-fg");
         WA.room.showLayer("facade");
-      });
+    });
+    WA.room.onEnterLayer("doorstep/zone_office_silent").subscribe(() => {
+        WA.room.showLayer("silentOverlay");
+    });
+    WA.room.onLeaveLayer("doorstep/zone_office_silent").subscribe(() => {
+        WA.room.hideLayer("silentOverlay");
+    });
     /*
     WA.room.onEnterLayer("doorstep/zone_office").subscribe(() => {
         const players = WA.players.list();
