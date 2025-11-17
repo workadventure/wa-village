47626330c47382efa4deb06f67900d86bd50f2ca
Hugo Averty <hugoaverty@gmail.com>
2025-11-17 15:59:35 +0100

    Winter edition

--- src/roofs.ts
+++ src/roofs.ts
@@ -65,18 +65,22 @@ WA.onInit().then(() => {
 const hideRoof1 = () => {
     WA.room.hideLayer("roof1");
     WA.room.hideLayer("sign1");
+    WA.room.hideLayer("winter2");
 }
 const showRoof1 = () => {
     WA.room.showLayer("roof1");
     WA.room.showLayer("sign1");
+    WA.room.showLayer("winter2");
 }
 const showRoof2 = () => {
     WA.room.showLayer("roof2");
     WA.room.showLayer("sign2");
+    WA.room.showLayer("winter2");
 }
 const hideRoof2 = () => {
     WA.room.hideLayer("roof2");
     WA.room.hideLayer("sign2");
+    WA.room.hideLayer("winter2");
 }
 
 export {}
